07b91b49ddafded628a93ba08eea4eb3498f2096
Clément Cahuzac <clement.cahuzac@gmail.com>
2022-02-04 10:15:20 +0100

    fix léger bug

--- DubiozaSite/script.js
+++ DubiozaSite/script.js
@@ -56,10 +56,53 @@ https://jsfiddle.net/jlratwil/w83BW/
 
 
 
+// changer l'icone menu entre croix et menu burger
+
+function changeImage() {
+  var image = document.getElementById('imageMenu1');
+  if (image.src.match("/DubiozaSite/cancel")) {
+      image.src = "../DubiozaSite/menu.png";
+  }
+  else {
+      image.src = "/DubiozaSite/cancel.png";
+  }
+}
 
 
 
 
+
+// diminuer taille header on scroll 
+
+window.onscroll = function() {functionScrollTop()};
+
+function functionScrollTop() {
+  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+    document.getElementById("logo").className = "headeronscroll"
+    document.getElementById("titreScroll").className = "titreScroll";
+  } else {
+    document.getElementById("logo").className = ""
+    document.getElementById("titreScroll").className = "";
+  }
+}
+
+
+
+
+// change size smoothly on scroll
+// installer JQuery / AJAX pour le faire fonctionner
+// https://stackoverflow.com/questions/60431903/how-to-make-image-size-change-smoothly-on-scroll
+
+$(function(){
+  $(window).scroll(function(){
+      if($(this).scrollTop() > 100){
+          $('logo').addClass('fixed-header');
+      }
+      else{
+          $('logo').removeClass('fixed-header');
+      }
+  });
+});
 
 
 
